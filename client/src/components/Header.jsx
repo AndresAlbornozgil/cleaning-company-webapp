c5b014b62d0c709b50c9fbbd0acb7f92a9de3e37
AA <aalborgil002@gmail.com>
2024-10-08 15:31:26 -0400

    Fixed order of links in NavBar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -52,8 +52,8 @@ const Header = () => {
         <ul className="hidden md:flex space-x-6 uppercase">
           <li className="cursor-pointer" onClick={handleHomeClick}>Home</li>  {/* HOME behavior handled here */}
           <li><Link to="/book-cleaning">Book Cleaning</Link></li>
-          <li className="cursor-pointer" onClick={() => handleNavClick(navigate, 'our-work')}>Our Work</li>  {/* Fix for OUR WORK scrolling */}
-          <li><Link to="/services">Services</Link></li>
+          <li><Link to="/services">Services</Link></li>  {/* Services moved before Our Work */}
+          <li className="cursor-pointer" onClick={() => handleNavClick(navigate, 'our-work')}>Our Work</li>  {/* Our Work moved after Services */}
           <li className="cursor-pointer" onClick={() => handleNavClick(navigate, 'reviews')}>Reviews</li>
           <li><Link to="/about">About</Link></li>
           <li className="cursor-pointer" onClick={() => handleNavClick(navigate, 'contact')}>Contact</li>
@@ -66,8 +66,8 @@ const Header = () => {
           <ul className="md:hidden absolute top-16 left-0 w-full bg-blue-600 text-white p-4 flex flex-col space-y-4">
             <li className="cursor-pointer" onClick={() => { handleHomeClick(); toggleMenu(); }}>Home</li>  {/* Mobile HOME link */}
             <li><Link to="/book-cleaning" onClick={toggleMenu}>Book Cleaning</Link></li>
-            <li className="cursor-pointer" onClick={() => { handleNavClick(navigate, 'our-work'); toggleMenu(); }}>Our Work</li>
             <li><Link to="/services" onClick={toggleMenu}>Services</Link></li>
+            <li className="cursor-pointer" onClick={() => { handleNavClick(navigate, 'our-work'); toggleMenu(); }}>Our Work</li>
             <li className="cursor-pointer" onClick={() => { handleNavClick(navigate, 'reviews'); toggleMenu(); }}>Reviews</li>
             <li><Link to="/about" onClick={toggleMenu}>About</Link></li>
             <li className="cursor-pointer" onClick={() => { handleNavClick(navigate, 'contact'); toggleMenu(); }}>Contact</li>
